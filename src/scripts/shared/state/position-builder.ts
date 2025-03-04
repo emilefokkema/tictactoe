b13c7e64d7e0a23279ac58f6ea18e19db5cbc535
Emile Fokkema <emilefokkema@gmail.com>
2025-03-04 20:53:53 +0100

    begin position builder

---
+++ src/scripts/shared/state/position-builder.ts
@@ -0,0 +1,82 @@
+import { PositionArray } from "./position-array";
+
+interface Section {
+    length: number
+    offset: number
+}
+
+const sections: Section[] = [
+    { length: 4, offset: 0},
+    { length: 4, offset: 4},
+    { length: 3, offset: 8},
+    { length: 3, offset: 11},
+    { length: 3, offset: 14},
+    { length: 3, offset: 17},
+    { length: 2, offset: 20},
+    { length: 2, offset: 22},
+    { length: 1, offset: 24}
+];
+
+export class PositionBuilder {
+    private constructor(
+        public positions: number,
+        public lastPlayedPosition: number | undefined,
+        public atEnd: boolean,
+        private readonly vacantPositions: PositionArray,
+        private readonly sectionIndex: number,
+        private readonly section: Section,
+    ){}
+
+    private readPositionIndex(positions: number): number | undefined{
+        if(this.atEnd){
+            return undefined;
+        }
+        const masked = (positions >> (this.section.offset)) & ((1 << this.section.length) - 1);
+        return masked === 0 ? undefined : Math.min(masked - 1, this.vacantPositions.length - 1);
+    }
+
+    private advance(position: number, positionIndex: number): PositionBuilder{
+        const newPositions = this.positions | positionIndex + 1 << this.section.offset;
+        const newAtEnd = this.sectionIndex === 8;
+        const newVacantPositions = this.vacantPositions.removeAtIndex(positionIndex);
+        const newSectionIndex = newAtEnd ? 8 : this.sectionIndex + 1;
+        const newSection = sections[newSectionIndex];
+        return new PositionBuilder(
+            newPositions,
+            position,
+            newAtEnd,
+            newVacantPositions,
+            newSectionIndex,
+            newSection
+        )
+    }
+
+    public write(position: number): PositionBuilder {
+        const positionIndex = this.vacantPositions.indexOf(position);
+        if(positionIndex === -1){
+            return this;
+        }
+        return this.advance(positionIndex, position);
+    }
+
+    public read(positions: number): PositionBuilder {
+        const positionIndex = this.readPositionIndex(positions);
+        if(positionIndex === undefined){
+            return this;
+        }
+        const position = this.vacantPositions.positionAt(positionIndex);
+        if(position === undefined){
+            return this;
+        }
+        return this.advance(positionIndex, position);
+    }
+
+    public static initial: PositionBuilder = new PositionBuilder(
+        0,
+        undefined,
+        false,
+        PositionArray.initial,
+        0, 
+        sections[0]
+    )
+}
